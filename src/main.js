83b5da0c3e1f05f3d9e931db1ccdd1fe2e2db033
AnimatorJoe <josephjin7@gmail.com>
2019-12-15 23:23:53 -0600

    setting up server side conf

--- src/main.js
+++ src/main.js
@@ -1,8 +1,18 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
+const io = require('socket.io')
 
-Vue.config.productionTip = false
+const socket = io('http://localhost:3000')
+socket.on('chat-message', (data) => {
+  console.log(data)
+})
+
+
+
+
+
+Vue.config.productionTip = true
 
 new Vue({
   router,
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,19 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Chatroom>
+    </Chatroom>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import Chatroom from '@/components/Chatroom.vue'
 
 export default {
   name: 'home',
   components: {
-    HelloWorld
+    Chatroom
   }
 }
 </script>
