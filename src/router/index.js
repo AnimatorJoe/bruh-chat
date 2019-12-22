d0cba7092bad32403054eb3345b48f068856eb71
AnimatorJoe <josephjin7@gmail.com>
2019-12-22 12:57:04 -0600

    adding new pages and updating routes

--- src/router/index.js
+++ src/router/index.js
@@ -1,23 +1,39 @@
-import Vue from 'vue'
-import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
+import Vue from "vue"
+import VueRouter from "vue-router"
+import Home from "../views/Home.vue"
+import About from "../views/About.vue"
+import Register from "../views/Register.vue"
+import Login from "../views/Login.vue"
+import Profile from "../views/Profile.vue"
 
 Vue.use(VueRouter)
 
 const routes = [
   {
-    path: '/',
-    name: 'home',
+    path: "/",
+    name: "home",
     component: Home
   },
   {
-    path: '/about',
-    name: 'about',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  }
+    path: "/about",
+    name: "about",
+    component: About
+  },
+  {
+    path: "/register",
+    name: "register",
+    component: Register
+  },
+  {
+    path: "/login",
+    name: "login",
+    component: Login
+  },
+  {
+    path: "/profile",
+    name: "profile",
+    component: Profile
+  },
 ]
 
 const router = new VueRouter({
