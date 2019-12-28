a1f3f762ff434949179950ffb4695e6ce47f605b
AnimatorJoe <josephjin7@gmail.com>
2019-12-27 23:48:51 -0600

    giving option to change your display name

--- src/views/Profile.vue
+++ src/views/Profile.vue
@@ -1,11 +1,16 @@
 <template>
     <div>
         Status:
-        <span v-if="loggedIn">Logged In</span>
-        <span v-else>Not Logged In</span>
+        <span v-if="loggedIn">
+            Logged In, this is your profile.
         <div>
+            <label>Username: </label>
+            <input type="text" v-model="displayName">
+            <button @click="updateProfile">Update Profile</button>
             <button @click="signOut">Sign Out</button>
         </div>
+        </span>
+        <span v-else>Not Logged In</span>
     </div>
 </template>
 
@@ -15,12 +20,22 @@ import "firebase/auth";
 
 export default {
     created() {
-        
+    },
+    data() {
+        return {
+            user: null,
+            displayName: null,
+        }
     },
     computed: {
         loggedIn() {
             return this.$store.state.loggedIn;
         }
+    },
+    created() {
+        this.user = firebase.auth().currentUser;
+        this.displayName = this.user.displayName;
+        console.log(this.user);
     },
     methods: {
         async signOut() {
@@ -28,6 +43,13 @@ export default {
                 const data = await firebase.auth().signOut();
                 console.log(data);
                 this.$router.replace({name:"login"});
+            } catch (err) {
+                console.log(err);
+            }
+        },
+        async updateProfile() {
+            try {
+                this.user.updateProfile({displayName: this.displayName});
             } catch (err) {
                 console.log(err);
             }
